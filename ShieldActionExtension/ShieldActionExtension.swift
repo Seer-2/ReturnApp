1325e0089e0ec1d8edd88d38c4d28506234a565c
Seer-2 <seerajsuper@gmail.com>
2026-09-01 18:25:04 -0400

    Add ShieldActionExtension/ShieldActionExtension.swift

---
+++ ShieldActionExtension/ShieldActionExtension.swift
@@ -0,0 +1,14 @@
+import ManagedSettings
+
+final class ShieldActionExtension: ShieldActionDelegate {
+    override func handle(action:ShieldAction,for application:ApplicationToken,completionHandler:@escaping(ShieldActionResponse)->Void){ completionHandler(response(for:action)) }
+    override func handle(action:ShieldAction,for category:ActivityCategoryToken,completionHandler:@escaping(ShieldActionResponse)->Void){ completionHandler(response(for:action)) }
+    override func handle(action:ShieldAction,for webDomain:WebDomainToken,completionHandler:@escaping(ShieldActionResponse)->Void){ completionHandler(response(for:action)) }
+    private func response(for action:ShieldAction)->ShieldActionResponse {
+        switch action {
+        case .primaryButtonPressed: return .openParentalControlsApp
+        case .secondaryButtonPressed: return .close
+        @unknown default: return .close
+        }
+    }
+}
